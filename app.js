5e48642b2822698896ff319ae02e367076b16780
Adam-Harte <adamharte11@hotmail.co.uk>
2020-05-03 00:22:47 +0100

    iterators added

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ user.uid = '1';
 console.log(user);
 console.log(Symbol('uid') === Symbol('uid'));
 console.log(user.toString());
+
+// iterators
+const company = {
+	curEmployee: 0,
+	employees: ['Adam', 'Edgar', 'Silv'],
+	next() {
+		if (this.curEmployee >= this.employees.length) {
+			return { value: this.curEmployee, done: true };
+		}
+		const returnValue = {
+			value: this.employees[this.curEmployee],
+			done: false,
+		};
+		this.curEmployee++;
+		return returnValue;
+	},
+};
+
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
+
+let employee = company.next();
+
+while (!employee.done) {
+	console.log(employee.value);
+	employee = company.next();
+}
